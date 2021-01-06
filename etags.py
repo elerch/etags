a8d567a384f22f79d1225250d277598b57200b82
Emil Lerch <emil@lerch.org>
2021-01-05 23:26:33 -0800

    add changed data to output for command line processing

--- etags.py
+++ etags.py
@@ -138,7 +138,8 @@ def lambda_handler(event, context):
 
     return {
         'statusCode': 200,
-        'body': json.dumps(event)
+        'body': json.dumps(event),
+        'changed': changed,
     }
 
 
